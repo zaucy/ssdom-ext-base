a11c8a01ccd27a49da3605d585ee3ee57ef4630b
zaucy <ezekiel@robotoppression.com>
2016-02-05 21:56:30 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const defaultScriptOptions = {
+	context: "client",
+	run_at: "start"
+};
+
+class RegisteredScript {
+
+	constructor(path, options) {
+		this.path = this.parsePath(path);
+		this.options = this.parseOptions(options);
+	}
+
+	parsePath(path) {
+		return path;
+	}
+
+	parseOptions(options) {
+		var parsedOptions = {};
+
+		for(let name in defaultScriptOptions) {
+			let option = options[name] || defaultScriptOptions[name];
+
+			parsedOptions[name] = this.parseOption(name, option);
+		}
+
+		return parsedOptions;
+	}
+
+	parseOption(name, value) {
+		switch(name) {
+			case 'context':
+				if(value !== "server-only"
+				&& value !== "server"
+				&& value !== "client") {
+					console.warn(`[ssdom-ext-base] Registered script context=${value} is invalid. context may only be 'server-only', 'server', or 'client'. Defaulting to 'client'.`);
+				}
+				break;
+			case 'run_at':
+				if(value !== "start"
+				&& value !== "end") {
+					console.warn(`[ssdom-ext-base] Registered script run_at=${value} is invalid. run_at may only be 'start' or 'end'. Defauling to 'start'.`);
+				}
+				break;
+			default:
+				throw Error(`Registered Script option '${name}' is invalid.`);
+		}
+
+		return value;
+	}
+};
+
+if(typeof module.parent._ssdomRegisteredScripts === "undefined") {
+	module.parent._ssdomRegisteredScripts = [];
+}
+delete require.cache[__filename];
+
+exports.registerScript = function(path, options) {
+	module.parent._ssdomRegisteredScripts.push(new RegisteredScript(path, options));
+};
